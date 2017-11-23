80c830ca5a40831ffcbd331c20a56282ae012e48
Michał Chudziak <michal.chudziak@gmail.com>
2017-11-23 14:10:22 +0100

    Revert "Update section-list.js"

--- src/section-list.js
+++ src/section-list.js
@@ -11,14 +11,12 @@ type Props = {
   onScrollBeginDrag?: (ScrollEvent) => any,
   onScroll?: (ScrollEvent) => any,
   onLayout?: (LayoutEvent) => any,
+  style?: Object,
 };
 
 export default class extends Component<Props> {
-  static defaultProps = {
-    onStartReachedThreshold: 0,
-  };
-  
   _onStartAlreadyCalled: boolean = false; // called already for a drag/momentum
+  _startThreshold: number = 0; // px from top onStartReached will be called
 
   componentDidUpdate = (prevProps, prevState) => {
     const oldSections = prevProps.sections;
@@ -39,7 +37,7 @@ export default class extends Component<Props> {
         this._listRef.scrollToLocation({
           sectionIndex: numAdded,
           itemIndex: 0,
-          viewOffset: 0, // get the user back to where they were
+          viewOffset: -currentPos, // get the user back to where they were
           animated: false,
         });
       }
@@ -50,7 +48,7 @@ export default class extends Component<Props> {
     <SectionList
       {...this.props}
       ref={ref => (this._listRef = ref)}
-      style={[{ flex: 1 }]}
+      style={[{ flex: 1 }, this.props.style || {}]}
       onLayout={this.onLayout}
       onMomentumScrollEnd={this.onMomentumScrollEnd}
       onScroll={this.onScroll}
@@ -72,9 +70,7 @@ export default class extends Component<Props> {
 
   onScroll = (e: ScrollEvent): void => {
     const { nativeEvent: { contentOffset: { y } } } = e;
-    if (this.props.onScroll && typeof this.props.onScroll === 'function') {
-      this.props.onScroll(e);
-    }
+    this.props.onScroll ? this.props.onScroll(e) : null;
 
     // XXX probably not the safest way to do this but ¯\_(ツ)_/¯
     const velocity = get(
@@ -83,7 +79,7 @@ export default class extends Component<Props> {
     );
 
     if (
-      y <= this.props.onStartReachedThreshold && // nearing the top
+      y <= this._startThreshold && // nearing the top
       velocity < 0 && // scrolling _toward_ the top
       !this._onStartAlreadyCalled && // hasn't been called this drag/momentum
       typeof this.props.onStartReached === 'function'
@@ -98,5 +94,8 @@ export default class extends Component<Props> {
     const { onLayout, onStartReachedThreshold } = this.props;
 
     onLayout ? onLayout(e) : null;
+
+    const threshold = onStartReachedThreshold ? onStartReachedThreshold : 0;
+    this._startThreshold = height * threshold;
   };
 }
